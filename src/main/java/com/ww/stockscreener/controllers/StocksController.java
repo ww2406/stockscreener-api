9f932696856e9fc2d0ea5b46ee008a6352f9c13e
William Welch <wwelchj@msn.com>
2023-10-20 01:41:40 -0700

    Configure provider to return data from Polygon API

--- src/main/java/com/ww/stockscreener/controllers/StocksController.java
+++ src/main/java/com/ww/stockscreener/controllers/StocksController.java
@@ -1,12 +1,9 @@
 package com.ww.stockscreener.controllers;
 
 import an.awesome.pipelinr.Pipeline;
-import an.awesome.pipelinr.Pipelinr;
 import com.ww.stockscreener.core.StockInfo;
 import com.ww.stockscreener.core.models.StockBar;
 import com.ww.stockscreener.data.pipelinr.commands.GetEntireMarketDataBarsCommand;
-import io.polygon.kotlin.sdk.rest.GroupedDailyParameters;
-import io.polygon.kotlin.sdk.rest.PolygonRestClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/stocks")
@@ -37,10 +31,6 @@ public class StocksController {
 
     @GetMapping("/stocks/cap")
     public List<StockInfo> getStockInfo() {
-        String polygonKey = System.getenv("POLYGON");
-        PolygonRestClient client = new PolygonRestClient(polygonKey);
-        var results = client.getGroupedDailyAggregatesBlocking(new GroupedDailyParameters("us","stocks", LocalDateTime.now().minusDays(5).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),false));
-        var infos = results.getResults().stream().map(dto -> new StockInfo(dto.getTicker(), dto.getClose() * dto.getVolume())).collect(Collectors.toList());
-        return infos;
+        return null;
     }
 }
